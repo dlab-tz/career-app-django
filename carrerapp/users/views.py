32ce8da89b7d03c0d8040d32f14d7342959a4eb7
heismussa <sms.mussasaid@gmail.com>
2025-08-04 15:30:42 +0300

    added view.py, urls.py and removed the duplicate forms

--- carrerapp/users/views.py
+++ carrerapp/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from .forms import UserProfileForm
 
 # Create your views here.
+def add_user_profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST)
+        if form.is_valid():
+         form.save()
+         return redirect('add_user_profile')
+        
+    else:
+        form = UserProfileForm()
+
+    return render(request, 'users/form.html',{'form': form})
