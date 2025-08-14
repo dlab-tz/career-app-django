b429e11ad163be8a54b5446ee2afe1aaec4efff4
Aminata Nyanda <aminatanyanda03@gmail.com>
2025-08-14 18:20:01 +0300

    Added country, region, district dropdowns

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     CAREER_CHOICES = [
@@ -34,14 +35,13 @@ class UserProfile(models.Model):
         ('masters', 'Masters Degree'),
         ('phd', 'PhD')
     ]
-
     name = models.CharField(max_length=100, default='Unknown')
-    dob = models.DateField(null=True, blank=True)  # allow null for existing records
-    region = models.CharField(max_length=50, default='Unknown Region')
-    district = models.CharField(max_length=50, default='Unknown District')
-    email = models.EmailField(default='unknown@example.com')
+    dob = models.DateField(null=True, blank=True) 
+    region = models.CharField(max_length=50, default='')
+    district = models.CharField(max_length=50, default='')
+    email = models.EmailField(default='')
     oversea = models.BooleanField(default=False)
-    country= models.CharField(max_length=100, blank=True, null=True)
+    country = CountryField(blank=True, null=True) 
     education_level = models.CharField(max_length=20, choices=EDUCATION_LEVEL_CHOICES, default='none')
     career_field = models.CharField(max_length=50, choices=CAREER_CHOICES, default='other')
     position = models.CharField(max_length=100, blank=True, null=True)
